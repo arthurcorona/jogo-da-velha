9e5c8342240b4154af855a93a5e80cf5f0988957
Arthur Corona <coronaggp@gmail.com>
2022-03-28 18:38:50 -0300

    correção

--- script/game.js
+++ script/game.js
@@ -6,14 +6,14 @@
     let symbols = ['apple', 'droid']
     let gameOver = false
     let winStates = [
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-        [0,3,6]
-        [1,4,7]
-        [2,5,8]
-        [0,4,8]
-        [2,4,6]
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
     ]
 
     function handleMove(position) {
